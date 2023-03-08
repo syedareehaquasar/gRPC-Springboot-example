bda7391cf5f404a42b2fc5140b4f4584545ad981
syedareehaquasar <reeha.saba@gmail.com>
2023-03-09 02:07:34 +0530

    adding stable version for sharing

---
+++ README.md
@@ -0,0 +1,4 @@
+## Commands used for client
+```
+curl 'http://localhost:8080/greeting?message=Hello from client'
+```
---
+++ greeting-service/src/main/java/com/springboot/grpc/service/GreetingServiceImpl.java
@@ -0,0 +1,21 @@
+package com.springboot.grpc.service;
+
+import com.springboot.grpc.*;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public void greeting(GreetingRequest request, StreamObserver<GreetingResponse> responseObserver) {
+        String message = request.getMessage();
+        System.out.println("Received Message: " + message);
+
+        GreetingResponse greetingResponse = GreetingResponse.newBuilder()
+                .setMessage("Received your " + message + ". Hello From Server. ")
+                .build();
+
+        responseObserver.onNext(greetingResponse);
+        responseObserver.onCompleted();
+    }
+}
